b4a4ce56c5a070bf198c134a824fedf5c261385d
shao0408 <42964187+shao0408@users.noreply.github.com>
2018-11-09 13:35:16 +0800

    优化
    优化

--- REST-Python2.7-demo/hbdm_api_demo.py
+++ REST-Python2.7-demo/hbdm_api_demo.py
@@ -9,7 +9,7 @@ python 2.7
 from HuobiDMService import HuobiDM
 from pprint import pprint
 
-URL = ""
+URL = "" #国内：api.dm.huobi.br.com  国外：api.dm.hbg.com
 
 ####  input your access_key and secret_key below:
 ACCESS_KEY = ''
