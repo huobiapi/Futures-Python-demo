e2cc0efe791b67d70469076f1c71e2fdc8fd1a84
shao0408 <42964187+shao0408@users.noreply.github.com>
2018-11-14 12:11:04 +0800

    优化
    优化

---
+++ Websocket-Python3-demo/README.md
@@ -0,0 +1,9 @@
+# Websocket-Python3-demo
+
+进入当前文件夹，安装WebSocketclient。pip3 install ./websocket_client-0.44.0-py2.py3-none-any.whl
+
+## 行情部分
+huobi.py
+
+## 账户相关
+orders-accounts-Python3-demo/
---
+++ Websocket-Python3-demo/huobi.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from websocket import create_connection
+import gzip
+import time
+
+if __name__ == '__main__':
+    while(1):
+        try:
+            ws = create_connection("wss://www.hbdm.com/ws")
+            break
+        except:
+            print('connect ws error,retry...')
+            time.sleep(5)
+
+    # 订阅 KLine 数据
+    
+    tradeStr_kline="""
+    {"sub": "market.BTC_CQ.kline.1min",  "id": "id1"}
+    """
+
+    # 订阅 Market Detail 数据
+    tradeStr_marketDetail="""
+    {"sub": "market.BTC_CQ.detail",  "id": "id6" }
+    """
+
+    # 订阅 Trade Detail 数据
+    tradeStr_tradeDetail="""
+    {"sub": "market.BTC_CQ.trade.detail", "id": "id7"}
+    """
+
+    # 请求 KLine 数据
+    tradeStr_klinereq="""
+    {"req": "market.BTC_CQ.kline.1min", "id": "id4"}
+    """
+
+    # 请求 Trade Detail 数据
+    tradeStr_tradeDetail_req="""
+    {"req": "market.BTC_CQ.kline.1min", "id": "id4"}
+    """
+
+    ws.send(tradeStr_kline)
+    trade_id = ''
+    while(1):
+        compressData=ws.recv()
+        result=gzip.decompress(compressData).decode('utf-8')
+        if result[:7] == '{"ping"':
+            ts=result[8:21]
+            pong='{"pong":'+ts+'}'
+            ws.send(pong)
+            ws.send(tradeStr_kline)
+        else:
+            try:
+                if trade_id == result['data']['id']:
+                    print('重复的id')
+                    break
+                else:
+                    trade_id = result['data']['id']
+            except Exception:
+                pass
+
+            print(result)
+
+    
